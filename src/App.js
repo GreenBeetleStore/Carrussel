52afdb3d19aef480914f686071e243a223b47d2c
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-11-20 20:09:56 +0100

    Carrussel bàsic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Carousel from './components/Carousel'
+const App = () => {
+   return (
+      <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
+         <Carousel>
+            <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+            <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+            <img src="https://via.placeholder.com/1600x300" alt="placeholder" />
+         </Carousel>
+      </div>
+   )
 }
 
-export default App;
+export default App
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import './carousel.css'
 
-const Carousel = () => {
-      return (
-            <div>
-                  
+const Carousel = (props) => {
+   const { children } = props
+   const [currentIndex, setCurrentIndex] = useState(0)
+   const [length, setLength] = useState(children.length)
+
+   // Estableix la longitud perquè coincideixi amb els "children" actuals dels "props".
+   useEffect(() => {
+      setLength(children.length)
+   }, [children])
+
+   const next = () => {
+      if (currentIndex < (length -1)) {
+            setCurrentIndex(prevState => prevState + 1)
+      }
+   }
+
+   const prev = () => {
+      if (currentIndex > 0) {
+            setCurrentIndex(prevState => prevState - 1)
+      }
+   }
+
+   return (
+      <div className="carousel-container">
+         <div className="carousel-wrapper">
+            <button onClick={prev} className="leftArrow">&lt;</button>
+            <div className="carousel-content-wrapper">
+               <div
+                  className="carousel-content"
+                  style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+               >
+                  {children}
+               </div>
             </div>
-      );
-};
+            <button onClick={next} className="rightArrow">&gt;</button>
+         </div>
+      </div>
+   )
+}
 
-export default Carousel;
+export default Carousel
